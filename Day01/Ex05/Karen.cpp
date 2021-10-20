b62ceb5f90440078a8fde150400ac70336d0e4a0
funiusa <funiusa@gmail.com>
2021-10-20 20:45:50 +0300

    remove if

--- Day01/Ex05/Karen.cpp
+++ Day01/Ex05/Karen.cpp
@@ -9,38 +9,39 @@ Karen::~Karen() {
 }
 
 void Karen::debug( void ) {
-	std :: cout << "I love to get extra bacon "
-				   "for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\n"
+	std :: cout << "I love to get extra bacon\n"
+	"for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\n"
 				   "I just love it!"
 				   << std::endl;
 }
 
 void Karen::info( void ) {
 	std::cout << "I cannot believe adding extra bacon cost more money.\n"
-				 "You don’t put enough!  If you did I would not have to ask for it!"
+	"You don’t put enough!  If you did I would not have to ask for it!"
 				 << std::endl;
 }
 
 void Karen::warning( void ) {
 	std :: cout << "I think I deserve to have some extra bacon for free.\n"
-				   "I’ve been coming here for years and you just started working here last month."
-				   << std::endl;
+	"I’ve been coming here for years\n"
+	"and you just started working here last month."
+	<< std::endl;
 }
 
 void Karen::error( void ) {
-	std :: cout << "This is unacceptable, I want to speak to the manager now."
-				<< std::endl;
+	std :: cout << "This is unacceptable,\n"
+	       "I want to speak to the manager now."
+		<< std::endl;
 }
 
 void Karen::complain( const std::string &level ) {
 	std::string lMass[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
-	void (Karen::*arrPtr[4])() = { &Karen::debug,
-								   &Karen::info,
-								   &Karen::warning,
-								   &Karen::error };
-	for (int le = 0; le < 4; le++) {
-		if (level == lMass[le]) {
-			(this->*arrPtr[le])(); break ;
-		}
-	}
+	void (Karen::*arrPtr[4])() = {	&Karen::debug,
+					&Karen::info,
+					&Karen::warning,
+					&Karen::error };
+	int le = 0;
+	for (; le < 4 && lMass[le] != level; le++)
+		;
+	return (this->*arrPtr[le])();
 }
