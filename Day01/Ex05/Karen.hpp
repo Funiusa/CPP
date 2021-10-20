971b9800fa836742d386167545a6f89d5ea9d182
funiusa <funiusa@gmail.com>
2021-10-20 13:59:30 +0300

    add Ex05 Karen2.0

---
+++ Day01/Ex05/Karen.hpp
@@ -0,0 +1,19 @@
+
+#ifndef EX05_KAREN_HPP
+#define EX05_KAREN_HPP
+
+#include <iostream>
+
+class Karen {
+private:
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
+public:
+	Karen();
+	~Karen();
+	void complain( const std::string &level );
+};
+
+#endif
